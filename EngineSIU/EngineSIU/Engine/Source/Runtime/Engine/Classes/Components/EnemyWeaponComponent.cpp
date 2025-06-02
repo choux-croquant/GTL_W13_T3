9f58d7c9b800dcbb0a0560dd241467778624ae02
choux-croquant <choifrance@gmail.com>
2025-06-02 18:02:32 +0900

    update

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/EnemyWeaponComponent.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/EnemyWeaponComponent.cpp
@@ -1,6 +1,18 @@
-﻿#include "EnemyWeaponComponent.h"
+#include "EnemyWeaponComponent.h"
+#include "Actors/Enemy.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Engine/Contents/AnimInstance/LuaScriptAnimInstance.h"
+#include "Animation/AnimStateMachine.h"
 
 void UEnemyWeaponComponent::GotParried(float InDamage)
 {
     UE_LOG(ELogLevel::Error,"GotParried");
+    AEnemy* EnemyActor = Cast<AEnemy>(GetOwner());
+
+    ULuaScriptAnimInstance* AnimInstance = Cast<ULuaScriptAnimInstance>(EnemyActor->SkeletalMeshComponent->GetAnimInstance());
+
+    if (UAnimStateMachine* StateMachine = AnimInstance->GetStateMachine())
+    {
+        StateMachine->State = FString("Reacting");
+    }
 }
