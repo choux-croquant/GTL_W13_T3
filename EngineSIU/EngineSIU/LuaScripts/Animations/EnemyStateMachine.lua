8292f1ec7c2818e440f0dd83bfe3ea12c5e94ff5
choux-croquant <choifrance@gmail.com>
2025-06-03 03:00:32 +0900

    add kneel animation and defeat state

--- EngineSIU/EngineSIU/LuaScripts/Animations/EnemyStateMachine.lua
+++ EngineSIU/EngineSIU/LuaScripts/Animations/EnemyStateMachine.lua
@@ -1,16 +1,21 @@
 AnimFSM = {
     currentState = "Idle",
+    idleAnimation = "Contents/Enemy_Idle/Armature|Enemy_Idle",
     attackAnimations = {
         "Contents/Horizontal1/Armature|Horizontal1",
         -- "Contents/Horizontal2/Armature|Horizontal2",
         -- "Contents/Vertical1/Armature|Vertical1",
     },
     reactionAnimation = "Contents/Enemy_Impact/Armature|Enemy_Impact",
+    kneelAnimation = "Contents/Kneel/Armature|Kneel",
+    kneelIdleAnimation = "Contents/Kneel_Idle/Armature|Kneel_Idle",
     attackCooldown = 3.0,
     isAttacking = false,
     isReacting = false,
+    isDefeated = false,
     lastAttackTime = 0,
     reactionEndTime = 0,
+    defeatStartTime = 0,
 
     TransitionToState = function(self, newState)
         self.currentState = newState
@@ -21,6 +26,8 @@ AnimFSM = {
             return self:HandleReactionState()
         elseif self.currentState == "Attacking" then
             return self:HandleAttackState()
+        elseif self.currentState == "Defeat" then
+            return self:HandleDefeatState()
         else
             return self:HandleIdleState(dt)
         end
@@ -33,7 +40,7 @@ AnimFSM = {
         end
 
         return {
-            anim = "Contents/Enemy_Idle/Armature|Enemy_Idle",
+            anim = idleAnimation,
             blend = 0.2,
             loop = true,
             rate_scale = 1.0,
@@ -83,6 +90,49 @@ AnimFSM = {
             rate_scale = 1.0,
             state = self.currentState
         }
+    end,
+
+    HandleDefeatState = function(self)
+        if not self.isDefeated then
+            -- 최초 진입 시 Kneel 애니메이션 실행
+            self.isDefeated = true
+            self.defeatStartTime = os.clock()
+            return {
+                anim = self.kneelAnimation,
+                blend = 0.3,
+                loop = false,
+                rate_scale = 1.0,
+                state = self.currentState
+            }
+        else
+            local kneelDuration = self.CurrentAnimDuration
+            -- local kneelDuration = 0.4 
+            -- if os.clock() > self.defeatStartTime + kneelDuration then
+            --     return {
+            --         anim = self.kneelIdleAnimation,
+            --         blend = 4.0,
+            --         loop = true,
+            --         rate_scale = 1.0,
+            --         state = self.currentState
+            --     }
+            if os.clock() > self.defeatStartTime + kneelDuration then
+                return {
+                    anim = self.kneelAnimation,
+                    blend = 0.0,
+                    loop = false,
+                    rate_scale = 1.0,
+                    state = self.currentState
+                }
+            -- else
+            --     return {
+            --     anim = self.kneelAnimation,
+            --     blend = 0.0,
+            --     loop = false,
+            --     rate_scale = 1.0,
+            --     state = self.currentState
+            -- }
+            end
+        end
     end,
 }
 
